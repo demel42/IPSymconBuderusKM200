cb220e57943c83b36a00174c72297c85a3b57f3e
demel42 <demel42@web.de>
2019-03-29 10:39:20 +0100

    erste commits

--- BuderusKM200/module.php
+++ BuderusKM200/module.php
@@ -13,6 +13,9 @@ class BuderusKM200 extends IPSModule
         $this->RegisterPropertyBoolean('module_disable', false);
 
         $this->RegisterPropertyString('host', '');
+		$this->RegisterPropertyInteger('port', 80);
+
+        $this->RegisterPropertyString('key', '');
 
         $this->RegisterPropertyInteger('update_interval', '60');
 
@@ -36,7 +39,9 @@ class BuderusKM200 extends IPSModule
         }
 
         $host = $this->ReadPropertyString('host');
-        if ($host == '') {
+        $port = $this->ReadPropertyInteger('port');
+        $key = $this->ReadPropertyString('key');
+        if ($host == '' || $port == 0 || $key == '') {
             $this->SetStatus(IS_INVALIDCONFIG);
         } else {
             $this->SetStatus(IS_ACTIVE);
@@ -58,6 +63,8 @@ class BuderusKM200 extends IPSModule
         $formElements[] = ['type' => 'CheckBox', 'name' => 'module_disable', 'caption' => 'Instance is disabled'];
         $formElements[] = ['type' => 'Label', 'label' => 'Buderus KM200'];
         $formElements[] = ['type' => 'ValidationTextBox', 'name' => 'host', 'caption' => 'Host'];
+		$formElements[] = ['type' => 'NumberSpinner', 'name' => 'port', 'caption' => 'Port'];
+        $formElements[] = ['type' => 'ValidationTextBox', 'name' => 'key', 'caption' => 'key'];
 
         $formElements[] = ['type' => 'Label', 'label' => 'Update status every X minutes'];
         $formElements[] = ['type' => 'NumberSpinner', 'name' => 'update_interval', 'caption' => 'Minutes'];
@@ -108,4 +115,100 @@ class BuderusKM200 extends IPSModule
 
         echo $msg;
     }
+
+	privat function Encrypt( $encryptData ) 
+	{ 
+		$key = $this->ReadPropertyString('key');
+
+		$blocksize = 16;
+		$encrypt_padchar = $blocksize - ( strlen( $encryptData ) % $blocksize ); 
+		$encryptData .= str_repeat( chr( $encrypt_padchar ), $encrypt_padchar ); 
+		
+		return base64_encode(
+			openssl_encrypt(
+				$encryptData,
+				"aes-256-ecb",
+				$key,
+				OPENSSL_RAW_DATA | OPENSSL_ZERO_PADDING
+			)
+		);
+	}
+
+	function Decrypt( $decryptData ) 
+	{ 
+		$key = $this->ReadPropertyString('key');
+
+		$decrypt = openssl_decrypt( 
+			base64_decode( $decryptData ),
+			"aes-256-ecb",
+			$key,
+			OPENSSL_RAW_DATA | OPENSSL_ZERO_PADDING
+		);
+			
+		$decrypt = rtrim( $decrypt, "\x00" ); 
+		$decrypt_len = strlen( $decrypt ); 
+		$decrypt_padchar = ord( $decrypt[ $decrypt_len - 1 ] ); 
+		for ( $i = 0; $i < $decrypt_padchar ; $i++ ) 
+		{ 
+			if ( $decrypt_padchar != ord( $decrypt[$decrypt_len - $i - 1] ) ) 
+			break; 
+		} 
+		if ( $i != $decrypt_padchar ) 
+			return $decrypt; 
+		else 
+			return substr( 
+				$decrypt, 
+				0, 
+				$decrypt_len - $decrypt_padchar 
+			); 
+	}
+
+	function GetData( $url ) 
+	{ 
+		$host = $this->ReadPropertyString('host');
+		$port = $this->ReadPropertyInteger('port');
+
+		$options = array( 
+			'http' => array( 
+			'method' => "GET", 
+			'header' => "Accept: application/json\r\n" . 
+							"User-Agent: TeleHeater/2.2.3\r\n" 
+			) 
+		); 
+		$context = stream_context_create( $options ); 
+		$content = @file_get_contents( 
+			'http://' . $host . ':' . $post . $url, 
+			false, 
+			$context 
+		); 
+		if ( false === $content ) 
+			return false; 
+		return json_decode( $this->Decrypt( $content ) ); 
+	} 
+
+	function SetData( $url, $Value ) 
+	{ 
+		$host = $this->ReadPropertyString('host');
+		$port = $this->ReadPropertyInteger('port');
+
+		$content = json_encode( 
+			array( 
+				"value" => $Value 
+			) 
+		); 
+		$options = array( 
+			'http' => array( 
+			'method' => "PUT", 
+				'header' => "Content-type: application/json\r\n" . 
+							"User-Agent: TeleHeater/2.2.3\r\n", 
+				'content' => $this->Encrypt( $content ) 
+			) 
+		); 
+		$context = stream_context_create( $options ); 
+		@file_get_contents( 
+			'http://' . $host . ':' . $port . $url, 
+			false, 
+			$context 
+		); 
+	}
 }
