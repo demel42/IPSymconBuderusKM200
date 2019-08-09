5b48af4ac52b2faeb20697ce5f7eaf794c1a94ff
IP-Symcon <root@ips-prod>
2019-08-09 14:47:15 +0200

    Schreibfehler korrigiert

--- libs/common.php
+++ libs/common.php
@@ -53,7 +53,7 @@ trait BuderusKM200Common
         return $ret;
     }
 
-    private function CreateVarProfile($Name, $ProfileType, $Suffix, $MinValue, $MaxValue, $StepSize, $Digits, $Icon, $Asscociations = '')
+    private function CreateVarProfile($Name, $ProfileType, $Suffix, $MinValue, $MaxValue, $StepSize, $Digits, $Icon, $Associations = '')
     {
         if (!IPS_VariableProfileExists($Name)) {
             IPS_CreateVariableProfile($Name, $ProfileType);
@@ -61,8 +61,8 @@ trait BuderusKM200Common
             IPS_SetVariableProfileValues($Name, $MinValue, $MaxValue, $StepSize);
             IPS_SetVariableProfileDigits($Name, $Digits);
             IPS_SetVariableProfileIcon($Name, $Icon);
-            if ($Asscociations != '') {
-                foreach ($Asscociations as $a) {
+            if ($Associations != '') {
+                foreach ($Associations as $a) {
                     $w = isset($a['Wert']) ? $a['Wert'] : '';
                     $n = isset($a['Name']) ? $a['Name'] : '';
                     $i = isset($a['Icon']) ? $a['Icon'] : '';
